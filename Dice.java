4956e9091cae67cb46f55aebb5391c3c8c86817b
Hyak1 <mask.k1@icloud.com>
2025-06-30 10:03:26 +0900

    Greetings added

--- Dice.java
+++ Dice.java
@@ -1,13 +1,23 @@
 import java.util.Random;
-public class Dice{
-    public static void main(String[] args){
-        int Dice1,Dice2,total;
-        Dice1 = new Random().nextInt(6) + 1;
-        Dice2 = new Random().nextInt(6) + 1;
-        total = Dice1 + Dice2;
+import java.util.Scanner;
+
+public class Dice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        System.out.print(">");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        
+        int dice1, dice2, total;
+        dice1 = new Random().nextInt(6) + 1;
+        dice2 = new Random().nextInt(6) + 1;
+        total = dice1 + dice2;
+        
         System.out.println("Rolling dice...");
-        System.out.println("Die 1: " + Dice1);
-        System.out.println("Die 2: " + Dice2);
+        System.out.println("Die 1: " + dice1);
+        System.out.println("Die 2: " + dice2);
         System.out.println("Total value: " + total);
+        scanner.close();
     }
 }
